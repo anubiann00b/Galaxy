665e00ad22ebc4dc7a0e9ec1c8f7b952e6181409
Shreyas <skraman1999@gmail.com>
2014-03-19 13:59:56 -0700

    Got rendering working (finally).

--- src/galaxy/Galaxy.java
+++ src/galaxy/Galaxy.java
@@ -2,14 +2,18 @@ package galaxy;
 
 import galaxy.render.VerticesManager;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.util.glu.GLU;
 
 public class Galaxy {
     
     public static void main(String[] args) {
         try {
-            Display.setDisplayMode(new DisplayMode(1366,768));
+            Display.setDisplayMode(new DisplayMode(800,600));
             Display.create();
         } catch (LWJGLException e) {
             System.out.println(e);
@@ -17,11 +21,87 @@ public class Galaxy {
         Display.setTitle("Galaxy");
         Galaxy galaxy = new Galaxy();
         galaxy.init();
+        
+        int delta = 0;
+        
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            long startTime = System.nanoTime();
+            galaxy.clearScreen();
+            Camera.update(delta);
+            galaxy.render();
+            Display.update();
+            long endTime = System.nanoTime();
+            Display.sync(60);
+            endTime = System.nanoTime();
+            delta = (int)((endTime - startTime)/1000000);
+        }
+        Display.destroy();
+        System.exit(0);
     }
     
-    private VerticesManager vertices;
+    private VerticesManager starVertices;
+    private int VBOVertexHandle;
+    private int VBOColorHandle;
+    
+    public final int points = 10000;
     
     private void init() {
-        vertices = new VerticesManager(100);
+        Camera.init();
+        this.initialize3D();
+        VBOVertexHandle = GL15.glGenBuffers();
+        VBOColorHandle = GL15.glGenBuffers();
+        
+        starVertices = new VerticesManager(points*3);
+        
+        for (int i=0;i<points;i++) {
+            starVertices.setColor((float)Math.random(),(float)Math.random(),(float)Math.random());
+            starVertices.addVertex((float)(Math.random()*10-5),(float)(Math.random()*10-5),(float)(Math.random()*10-5));
+        }
+        
+        starVertices.getPositionData().flip();
+        starVertices.getColorData().flip();
+        
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOVertexHandle);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER,starVertices.getPositionData(),GL15.GL_STREAM_DRAW);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOColorHandle);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER,starVertices.getColorData(),GL15.GL_STREAM_DRAW);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,0);
+    }
+    
+    private void render() {
+        GL11.glPushMatrix();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOVertexHandle);
+        GL11.glVertexPointer(3,GL11.GL_FLOAT,0,0L);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOColorHandle);
+        GL11.glColorPointer(3,GL11.GL_FLOAT,0,0L);
+        GL11.glDrawArrays(GL11.GL_POINTS,0,points);
+        GL11.glPopMatrix();
+    }
+    
+    public void clearScreen() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glLoadIdentity();
+    }
+    
+    public void initialize3D() {
+        GL11.glShadeModel(GL11.GL_SMOOTH); // Smoother textures.
+        GL11.glClearColor(0.0f,0.0f,0.0f,0.0f); // BG color. 6698FF
+        GL11.glClearDepth(1.0); // Buffer depth, allows objects to draw over things behind them.
+        GL11.glEnable(GL11.GL_DEPTH_TEST); // Depth testing (see above).
+        GL11.glDepthFunc(GL11.GL_LEQUAL); // Type of depth testing.
+        
+        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY); // Needed for VBO's.
+        GL11.glEnableClientState(GL11.GL_COLOR_ARRAY); // Needed to send color with VBO's.
+        
+        GL11.glMatrixMode(GL11.GL_PROJECTION); // Sets matrix mode to displaying pixels.
+        GL11.glLoadIdentity(); // Loads the above matrix mode.
+        
+        // Sets default perspective location.                       Render Distances: Min   Max
+        GLU.gluPerspective(45.0f,(float)Display.getWidth()/(float)Display.getHeight(),0.1f,300.0f);
+        
+        GL11.glMatrixMode(GL11.GL_MODELVIEW); // Sets the matrix to displaying objects.
+        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT,GL11.GL_NICEST); // Something for quality.
+        GL11.glEnable(GL11.GL_CULL_FACE); // Efficient rendering.
     }
 }
--- src/galaxy/render/VerticesManager.java
+++ src/galaxy/render/VerticesManager.java
@@ -12,14 +12,20 @@ public class VerticesManager {
     private FloatBuffer vertexBuffer;
 	private FloatBuffer colorBuffer;
     
-    public VerticesManager(int length) {
+    private int length = 0;
+    
+    public VerticesManager(int newLength) {
         r = 0;
         g = 0;
         b = 0;
         
+        this.length = newLength;
+        
         vertexBuffer = BufferUtils.createFloatBuffer(length);
         colorBuffer = BufferUtils.createFloatBuffer(length);
     }
+    
+    public int size() { return length; }
     
     public void setColor(float r, float g, float b) {
         this.r = r;
@@ -27,7 +33,7 @@ public class VerticesManager {
         this.b = b;
     }
     
-    public void addVertex(int x, int y, int z) {
+    public void addVertex(float x, float y, float z) {
         vertexBuffer.put(x);
 		vertexBuffer.put(y);
 		vertexBuffer.put(z);
@@ -35,5 +41,13 @@ public class VerticesManager {
 		colorBuffer.put(r);
 		colorBuffer.put(g);
 		colorBuffer.put(b);
+    }
+    
+    public FloatBuffer getPositionData() {
+        return vertexBuffer;
+    }
+    
+    public FloatBuffer getColorData() {
+        return colorBuffer;
     }
 }
